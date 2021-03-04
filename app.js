1b83e7a7c4ef0788e67860de837fda78be133ebb
Lucas Tonetto Firmo <lucasfirmo@hotmail.com>
2021-03-04 19:23:16 -0300

    feat: Create sudoku and calculate line option values
    Create object that represents sudoku into javascript
    And create a function that calculates validy values for
    line

--- app.js
+++ app.js
@@ -8,4 +8,44 @@ window.onload = () => {
 			}
 		});
 	});
+
+	const createSudoku = () => {
+		const sudoku = [];
+		document.querySelectorAll('.input_number').forEach(elm => {
+			const [square, line, column] = elm.id.split('');
+			sudoku.push({
+				line,
+				column,
+				square,
+				value: elm.value,
+				valueOptions: {
+					line: [],
+					column: [],
+					square: []
+				}
+			});
+		});
+		return sudoku;
+	};
+
+	const putLineValueOptions = (sudoku) => {
+		const newSudoku = [];
+		const values = lines =  [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		lines.forEach(lineNumber => {
+			const line = sudoku.filter(lineValue => lineValue.line == lineNumber);
+			const numbersAtLine = line.map(cell => cell.value).filter(number => number).map(number => parseInt(number));
+			const lineOptions = values.filter(value => numbersAtLine.indexOf(value) === -1);
+			line.forEach(cell => {
+				cell.valueOptions.line = lineOptions;
+				newSudoku.push(cell);
+			})
+		});
+		return newSudoku;
+	};
+
+	document.querySelector('#solve').addEventListener('click', () => {
+		const sudoku = createSudoku();
+		putLineValueOptions(sudoku);
+		
+	});
 }
