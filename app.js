c7051f87e1b33c60bb1aaac9d6f69b9055b1ea29
Lucas Tonetto Firmo <lucasfirmo@hotmail.com>
2021-03-07 00:32:08 -0300

    feat: Calculate possible values at regions and put it into the cells
    Create function that calculate availables values for each cell
    And create function that put the number into the cell

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 window.onload = () => {
-	document.querySelectorAll('.input_number').forEach(elm => {
-		elm.addEventListener('keyup', function(e) {
-			if(/[1-9]/.test(e.key)) {
-				elm.value = e.key;
-			} else {
-				elm.value = '';
-			}
-		});
-	});
+	// document.querySelectorAll('.input_number').forEach(elm => {
+	// 	elm.addEventListener('keyup', function(e) {
+	// 		if(/[1-9]/.test(e.key)) {
+	// 			elm.value = e.key;
+	// 		} else {
+	// 			elm.value = '';
+	// 		}
+	// 	});
+	// });
 
 	const createSudoku = () => {
 		const sudoku = [];
@@ -28,24 +28,54 @@ window.onload = () => {
 		return sudoku;
 	};
 
-	const putLineValueOptions = (sudoku) => {
-		const newSudoku = [];
-		const values = lines =  [1, 2, 3, 4, 5, 6, 7, 8, 9];
-		lines.forEach(lineNumber => {
-			const line = sudoku.filter(lineValue => lineValue.line == lineNumber);
-			const numbersAtLine = line.map(cell => cell.value).filter(number => number).map(number => parseInt(number));
-			const lineOptions = values.filter(value => numbersAtLine.indexOf(value) === -1);
-			line.forEach(cell => {
-				cell.valueOptions.line = lineOptions;
-				newSudoku.push(cell);
-			})
+	const putValueOptions = (sudoku) => {
+		const newSudoku = [...sudoku];
+		const values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		const regions = ['line', 'column', 'square'];
+		regions.forEach(region => {
+			values.forEach(value => {
+				const especificRegion = newSudoku.filter(cellValue => cellValue[region] == value);
+				const numbersAtRegion = especificRegion.map(cell => cell.value)
+					.filter(number => number)
+					.map(number => parseInt(number));
+				const regionOptins = values.filter(numberValue => numbersAtRegion.indexOf(numberValue) === -1);
+				especificRegion.forEach(cell => {
+					cell.valueOptions[region] = regionOptins;
+				});
+			});
 		});
 		return newSudoku;
 	};
 
+	const defineValues = (sudoku) => {
+		const newSudoku = [];
+		const values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		sudoku.filter(cell => !cell.value).forEach(cell => {
+			const possibleNumbers = values.filter(number => 
+				cell.valueOptions.line.indexOf(number) > -1 &&
+				cell.valueOptions.column.indexOf(number) > -1 &&
+				cell.valueOptions.square.indexOf(number) > -1
+			);
+			if(possibleNumbers.length === 1) {
+				cell.value = possibleNumbers[0];
+			}
+			newSudoku.push(cell);
+		});
+		return newSudoku;
+	};
+
+	const putValues = (sudoku) => {
+		sudoku.forEach(cell => {
+			document.getElementById(''+cell.square+cell.line+cell.column).value = cell.value;
+		});
+	};
+
 	document.querySelector('#solve').addEventListener('click', () => {
-		const sudoku = createSudoku();
-		putLineValueOptions(sudoku);
-		
+		let sudoku = createSudoku();
+		for(let i = 0; i < 1; i++) {
+			sudoku = defineValues(putValueOptions(sudoku));
+			console.log(sudoku);
+			putValues(sudoku);
+		}
 	});
 }
